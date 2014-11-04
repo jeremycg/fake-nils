96f9a87b9295c5afe11d52273964939b23614e5b
jeremycg <jeremycgray@gmail.com>
2014-11-04 16:22:34 -0500

    add example usage to fakeparent.py

--- fakeparent.py
+++ fakeparent.py
@@ -1,8 +1,8 @@
 #takes output of align.sh, changes it from a fastq to a fasta, then fills in ns
-#ns are taken from the reference genome
+#ns are taken from the reference genome at the same location
 #indels are removed in the vcutils step to ensure direct synteny
 #usage: fakeparent.py input.fq refparent.fa output.fa 
-
+#example usage: fakeparent.py fake1.fq parent1_ref.fa fakedp2.fa
 
 import Bio
 import sys
