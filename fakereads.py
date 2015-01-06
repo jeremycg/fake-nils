af03c761a0a681c76677747028d91556d9d7d8f4
jeremycg <jeremycgray@gmail.com>
2015-01-06 16:00:22 -0500

    speed ups in fakereads.py
    two main speed ups - previously was opening entire input file using list
    - not usable with larger files (memory). Second speed up is removing the
    opening and closing of the outputfile from the loop - now only opened
    and closed once.

--- fakereads.py
+++ fakereads.py
@@ -2,7 +2,7 @@
 #usage fakereads.py inputtxt outputfastq coverage quality
 #example fakereads.py outp2.txt outp2.fastq 10 40
 
-import Bio
+from Bio import SeqIO
 import sys
 
 
@@ -10,12 +10,12 @@ def writefastq(sequence,outfile,coverage,quality):
 	rec = sequence
 	rec.letter_annotations["phred_quality"] = [quality] * len(sequence.seq)
 	recs = [ rec ]
-	handle = open(outfile, "a")
 	for i in range(coverage):
-		SeqIO.write(recs, handle, "fastq")
+		SeqIO.write(recs, outfile, "fastq")
 
 handle = open(sys.argv[1], "rU")
-records = list(SeqIO.parse(handle, "fasta"))
+handleout = open(sys.argv[2], "a")
+for i in SeqIO.parse(handle, "fasta"):
+	writefastq(i,handleout,int(sys.argv[3]),int(sys.argv[4]))
 handle.close()
-for i in records:
-	writefastq(i,sys.argv[2],int(sys.argv[3]),int(sys.argv[4]))
+handleout.close()
